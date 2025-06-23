c9cf20f84e248b9c881026bc17131d48780eba4c
Alex <AlexKozlovDeveloper@gmail.com>
2025-06-24 00:23:50 +0300

    Update CreateUserDataModel.cs

--- src/HomeTask/HomeTask.Core/Models/CreateUserDataModel.cs
+++ src/HomeTask/HomeTask.Core/Models/CreateUserDataModel.cs
@@ -1,4 +1,6 @@
-﻿namespace HomeTask.Core.Models;
+﻿using System.Text.RegularExpressions;
+
+namespace HomeTask.Core.Models;
 
 public class CreateUserDataModel
 {
@@ -6,4 +8,31 @@ public class CreateUserDataModel
     public string Email { get; set; }
     public string Password { get; set; }
     public UserRole Role { get; set; }
+
+    public void Validate()
+    {
+        if (string.IsNullOrWhiteSpace(Name)) 
+        {
+            throw new Exception($"{nameof(Name)} can't be empty");
+        }
+
+        if (string.IsNullOrWhiteSpace(Email))
+        {
+            throw new Exception($"{nameof(Name)} can't be empty");
+        }
+
+        var emailRegex = new Regex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+
+        if (!emailRegex.IsMatch(Email))
+        {
+            throw new Exception($"Invalid email");
+        }
+
+        var passwordRegex = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$");
+
+        if (!passwordRegex.IsMatch(Password))
+        {
+            throw new Exception($"Invalid Password! Password must contains: minimum 8 characters, at least one uppercase letter(A-Z), at least one lowercase letter(a-z), at least one number(0-9), at least one special character e.g. !@#$%^&*()");
+        }
+    }
 }
