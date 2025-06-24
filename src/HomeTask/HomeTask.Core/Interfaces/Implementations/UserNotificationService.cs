58535f58d047e6f544fbb16624afaf67c62cd77a
Alex <AlexKozlovDeveloper@gmail.com>
2025-06-25 02:28:55 +0300

    Create UserNotificationService.cs

---
+++ src/HomeTask/HomeTask.Core/Interfaces/Implementations/UserNotificationService.cs
@@ -0,0 +1,11 @@
+﻿using HomeTask.Core.Models;
+
+namespace HomeTask.Core.Interfaces.Implementations;
+
+public class UserNotificationService : IUserNotificationService
+{
+    public Task NotifyUserUpdated(UserDto user)
+    {
+        return Task.CompletedTask;
+    }
+}
