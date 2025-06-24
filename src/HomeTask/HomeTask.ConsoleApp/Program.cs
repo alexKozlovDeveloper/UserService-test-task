8341e82db4e5db9af93331ec94d462cfc405c4cc
Alex <AlexKozlovDeveloper@gmail.com>
2025-06-25 02:28:27 +0300

    Update Program.cs

--- src/HomeTask/HomeTask.ConsoleApp/Program.cs
+++ src/HomeTask/HomeTask.ConsoleApp/Program.cs
@@ -1,26 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using HomeTask.Core.Infrastructure.Database;
-using HomeTask.Core.Models;
-using Microsoft.EntityFrameworkCore;
 
 Console.WriteLine("Home Task");
-
-var db = new HomeTaskDbContext();
-
-var user = new User 
-{ 
-    Name = "name",
-    Email = "email",
-    PasswordHash = "gr46#",
-    Role = UserRole.User
-};
-
-db.Users.Add(user);
-await db.SaveChangesAsync();
-
-var users = await db.Users.ToListAsync();
-
-foreach (var item in users)
-{
-    Console.WriteLine(item.Name);
-}
