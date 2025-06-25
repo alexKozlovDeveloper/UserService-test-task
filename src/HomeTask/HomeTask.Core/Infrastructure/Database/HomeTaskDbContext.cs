2a55605a68f690f11b218a73bd2ce63c632fb04e
Alex <AlexKozlovDeveloper@gmail.com>
2025-06-26 01:33:16 +0300

    Update HomeTaskDbContext.cs

--- src/HomeTask/HomeTask.Core/Infrastructure/Database/HomeTaskDbContext.cs
+++ src/HomeTask/HomeTask.Core/Infrastructure/Database/HomeTaskDbContext.cs
@@ -7,22 +7,8 @@ public class HomeTaskDbContext : DbContext
 {
     public DbSet<User> Users { get; set; }
 
-    //public string DbPath { get; } = string.Empty;
-
     public HomeTaskDbContext(DbContextOptions<HomeTaskDbContext> options) : base(options)
     {
 
     }
-
-    //public HomeTaskDbContext()
-    //{
-    //    var folder = Environment.SpecialFolder.LocalApplicationData;
-    //    var path = Environment.GetFolderPath(folder);
-    //    DbPath = Path.Join(path, "hometask.db");
-    //}
-
-    //// The following configures EF to create a Sqlite database file in the
-    //// special "local" folder for your platform.
-    //protected override void OnConfiguring(DbContextOptionsBuilder options)
-    //    => options.UseSqlite($"Data Source={DbPath}");
 }
