56a82da7f2894db97a649189b0961c84037a7cff
Alex <AlexKozlovDeveloper@gmail.com>
2025-06-26 01:34:01 +0300

    Move Subscribe method to user controller

--- src/HomeTask/HomeTask.WebApi/Controllers/UserController.cs
+++ src/HomeTask/HomeTask.WebApi/Controllers/UserController.cs
@@ -3,12 +3,13 @@ using HomeTask.Core.Interfaces;
 using HomeTask.Core.Models.Request;
 using HomeTask.Core.Models.Response;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace HomeTask.WebApi.Controllers;
 
 [ApiController]
 [Route("users")]
-public class UserController(IUserService UserService) : ControllerBase
+public class UserController(IUserService UserService, IWebSocketConnectionManager ConnectionManager) : ControllerBase
 {
     [HttpGet]
     public async Task<IEnumerable<UserResponseModel>> GetUsersAsync(CancellationToken ct)
@@ -33,5 +34,25 @@ public class UserController(IUserService UserService) : ControllerBase
         )
     {
         await UserService.UpdateUserRoleAsync(userId, newRole, ct);
+    }
+
+    [HttpGet("{userId}:subscribe")]
+    public async Task Subscribe(int userId)
+    {
+        if (HttpContext.WebSockets.IsWebSocketRequest)
+        {
+            var socket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+
+            ConnectionManager.AddSocket(userId, socket);
+
+            while (socket.State == System.Net.WebSockets.WebSocketState.Open)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+        else
+        {
+            HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+        }
     }
 }
